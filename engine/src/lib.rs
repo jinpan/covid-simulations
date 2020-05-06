25e79f9f4967ccee627d97b4c62ea91296421ea1
Jin Pan <jinxp1@gmail.com>
2020-05-06 13:37:57 -0700

    WEB: initialize wasm-app template for frontend

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -9,9 +9,12 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    log(&format!("Hello, {}!", name));
 }
