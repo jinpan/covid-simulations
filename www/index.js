de3980ed830eb062112c0311bfc9705bdd0a6901
Jin Pan <jinxp1@gmail.com>
2020-05-08 11:28:15 -0700

    WEB: integrate engine with 3js renderer

--- www/index.js
+++ www/index.js
@@ -3,41 +3,43 @@ import * as wasm from "wasm-engine";
 // TODO: remove this debugging hack.
 window.WASM_ENGINE = wasm;
 
-wasm.foo();
+var world = wasm.create_world({
+  "disease_parameters": {
+    "infection_radius": 18,
+      "infectious_period_ticks": 10 * 60 ,
+  },
+  "size": [600, 400],
+  "num_people": 200,
+  "num_initially_infected": 1,
+});
+window._WORLD = world;
 
 import * as THREE from 'three';
 window.THREE = THREE;
 
 var scene = new THREE.Scene();
 
-var geometry = new THREE.CircleGeometry( 5, 32 );
-var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-var circle = new THREE.Mesh( geometry, material );
-scene.add( circle );
-var circle_right = true;
+var geometry = new THREE.CircleGeometry( 3, 8 );
 
-function animate_circle() {
-  setTimeout(animate_circle, 16);
-  if (circle_right) {
-    if (circle.position.x < 100) {
-      circle.position.x += 1;
-    } else {
-      circle_right = false;
-    }
-  } else {
-    if (circle.position.x > -100) {
-      circle.position.x -= 1;
-    } else {
-      circle_right = true;
-    }
-  }
+let circlesByID = new Map();
+window._CID = circlesByID;
+for (const person of world.to_json()["people"]) {
+  let material = new THREE.MeshBasicMaterial( { color: person.color } );
+  let circle = new THREE.Mesh( geometry, material );
+
+  circle.position.x = person.position_x;
+  circle.position.y = person.position_y;
+  // circle.color = new THREE.Color(person.color);
+
+  circlesByID.set(person.id, circle);
+  scene.add(circle);
 }
-animate_circle();
 
-const width = 400;
-const height = 300;
+const width = 600;
+const height = 400;
+const pad = 5;
 var camera = new THREE.OrthographicCamera(
-  width / - 2, width / 2, height / 2, height / - 2,
+  0 - pad, width + pad, height + pad, 0 - pad,
   1, 1000);
 camera.position.z = 5;
 
@@ -46,6 +48,17 @@ var renderer = new THREE.WebGLRenderer({ "canvas": canvas });
 renderer.setClearColor (0xf0f0f0, 1);
 
 function animate() {
+  world.step();
+
+  for (const person of world.to_json()["people"]) {
+    let circle = circlesByID.get(person.id);
+
+    circle.position.x = person.position_x;
+    circle.position.y = person.position_y;
+    circle.material.color.setHex(person.color);
+    // circle.material.color.setHex(0x0000ff);
+  }
+
   requestAnimationFrame( animate );
   renderer.render( scene, camera );
 }
